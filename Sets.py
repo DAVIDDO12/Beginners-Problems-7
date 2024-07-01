5322cf1411120b54dac6a26728fe2d507f5f4992
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-07-01 17:14:41 +1200

    Update Sets.py

--- Sets.py
+++ Sets.py
@@ -7,3 +7,19 @@ for num in numsList:
         print("NO")
         seen.add(num)
 
+String = input("Enter a phrase:")
+Words = set(String.split())
+A = sum(word.count("a")for word in Words)
+print(A)
+
+list1 = input("Enter numbers for the first list, separated by spaces: ").split()
+list2 = input("Enter numbers for the second list, separated by spaces: ").split()
+list1 = [int(num) for num in list1]
+list2 = [int(num) for num in list2]
+commonnumbers = set(list1) & set(list2)
+commonnumbers = list(commonnumbers)
+for integer in range(len(commonnumbers)):
+    for number in range(integer+1,len(commonnumbers)):
+        if commonnumbers[integer]>commonnumbers[number]:
+            commonnumbers[integer],commonnumbers[number] = commonnumbers[number],commonnumbers[integer]
+print(commonnumbers)
